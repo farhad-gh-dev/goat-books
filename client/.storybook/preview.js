a3817f986cfac9de0280e672d3f2f5f3d8fdc823
Farhad Gholami <farhad.dev.contact@gmail.com>
2022-10-08 23:00:11 +0330

    Add theming to storybook

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,3 +1,39 @@
+import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
+import { addDecorator } from "@storybook/react";
+import { withThemesProvider } from "storybook-addon-styled-component-theme";
+import { ThemeProvider } from "styled-components";
+import { StorybookGlobalStyle } from "../src/Styles/global";
+import { themes } from "../src/Styles/index";
+
+const customViewports = {
+  ...MINIMAL_VIEWPORTS,
+  LaptopScreen: {
+    name: "Laptop Screen",
+    styles: {
+      width: "1280px",
+      height: "800px",
+    },
+  },
+  LargeScreen: {
+    name: "Large Screen",
+    styles: {
+      width: "1920px",
+      height: "1080px",
+    },
+  },
+};
+
+const allThemes = [{ ...themes.light }, { ...themes.dark }];
+addDecorator(withThemesProvider(allThemes), ThemeProvider);
+
+export const decorators = [
+  (Story) => (
+    <>
+      <StorybookGlobalStyle />
+      <Story />
+    </>
+  ),
+];
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +42,7 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+  viewport: {
+    viewports: customViewports,
+  },
+};
