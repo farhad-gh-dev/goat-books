832260866ccb40ba7f1b7e41e60451f8cbf5faf5
Farhad Gholami <farhad.dev.contact@gmail.com>
2022-10-13 21:06:29 +0330

    Update components index exports

---
+++ client/src/Components/index.ts
@@ -0,0 +1,10 @@
+// Atoms
+export * from "./Atoms/BrandLogo/BrandLogo";
+export * from "./Atoms/Button/Button";
+export * from "./Atoms/Heading/Heading";
+export * from "./Atoms/Link/Link";
+export * from "./Atoms/Text/Text";
+export * from "./Atoms/TextInput/TextInput";
+
+// Molecules
+export * from "./Organisms/SignInForm/SignInForm";
