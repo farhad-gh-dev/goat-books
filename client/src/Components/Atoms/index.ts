0bca87d2c794d73adfebb21894d8313bd9a77a4d
Farhad Gholami <farhad.dev.contact@gmail.com>
2022-10-17 19:57:59 +0330

    Update export index files to prevent circular imports

---
+++ client/src/Components/Atoms/index.ts
@@ -0,0 +1,6 @@
+export * from "./BrandLogo/BrandLogo";
+export * from "./Button/Button";
+export * from "./Heading/Heading";
+export * from "./Link/Link";
+export * from "./Text/Text";
+export * from "./TextInput/TextInput";
--- client/src/Components/index.ts
+++ client/src/Components/index.ts
@@ -1,10 +1,2 @@
-// Atoms
-export * from "./Atoms/BrandLogo/BrandLogo";
-export * from "./Atoms/Button/Button";
-export * from "./Atoms/Heading/Heading";
-export * from "./Atoms/Link/Link";
-export * from "./Atoms/Text/Text";
-export * from "./Atoms/TextInput/TextInput";
-
-// Molecules
-export * from "./Organisms/SignInForm/SignInForm";
+export * from "./Atoms";
+export * from "./Organisms";
