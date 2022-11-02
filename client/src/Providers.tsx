5b0be189cbfc8c8d1163f1c026c4c6991f74cde8
Farhad Gholami <farhad.dev.contact@gmail.com>
2022-11-02 20:16:23 +0330

    Add react query library

--- client/src/Providers.tsx
+++ client/src/Providers.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "Styles/global";
 import { AppThemeProvider } from "Contexts/AppThemeContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const AppProvider: React.FC<{ children: any }> = ({ children }) => {
   return (
     <BrowserRouter>
-      <AppThemeProvider>
-        <GlobalStyle />
-        {children}
-      </AppThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppThemeProvider>
+          <GlobalStyle />
+          {children}
+        </AppThemeProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
