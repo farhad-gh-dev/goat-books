9e297b2a192c822f6c9c8ad2b739b93d80a7aad4
Farhad Gholami <farhad.dev.contact@gmail.com>
2022-11-04 20:42:14 +0330

    Add hex color to rgb(a) mixin

--- client/src/Styles/mixins.ts
+++ client/src/Styles/mixins.ts
@@ -14,3 +14,23 @@ export const selectFontWeight = (fontWeight?: string) => {
       }`
     : `font-weight: ${fontWeight}`;
 };
+
+export const hexToRgb = (hex?: string, opacity?: number): string => {
+  if (!hex) return "";
+
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+  if (!result) return "";
+
+  if (opacity) {
+    return `rgba(${parseInt(result[1], 16)}, ${parseInt(
+      result[2],
+      16
+    )}, ${parseInt(result[3], 16)}, ${opacity})`;
+  }
+
+  return `rgb(${parseInt(result[1], 16)}, ${parseInt(
+    result[2],
+    16
+  )}, ${parseInt(result[3], 16)})`;
+};
