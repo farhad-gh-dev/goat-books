c66848d47414eb9cafb68edf506f90d54db3a951
Farhad Gholami <farhad.dev.contact@gmail.com>
2023-07-01 21:50:37 +0330

    Update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(Number(process.env.APP_PORT || 3000));
-  console.log(`App is running on: ${app.getUrl()}`);
+  console.log(`App is running on: ${JSON.stringify(await app.getUrl())}`);
 }
 bootstrap();
